3ef1513163ea70f98458f7c379bb3a1e28d4fcfc
jack <jack.wgm@gmail.com>
2019-03-05 11:21:08 +0800

    add rpc_error_code

---
+++ include/tinyrpc/rpc_error_code.hpp
@@ -0,0 +1,85 @@
+﻿//
+// Copyright (C) 2019 Jack.
+//
+// Author: jack
+// Email:  jack.wgm at gmail dot com
+//
+
+#pragma once
+
+#include "boost/system/system_error.hpp"
+#include "boost/system/error_code.hpp"
+
+namespace tinyrpc {
+
+	//////////////////////////////////////////////////////////////////////////
+	namespace detail {
+		class error_category_impl;
+	}
+
+	template<class error_category>
+	const boost::system::error_category& error_category_single()
+	{
+		static error_category error_category_instance;
+		return reinterpret_cast<const boost::system::error_category&>(error_category_instance);
+	}
+
+	inline const boost::system::error_category& error_category()
+	{
+		return error_category_single<detail::error_category_impl>();
+	}
+
+	namespace errc {
+		enum errc_t
+		{
+			parse_rpc_service_ptl_failed = 1,
+			unknow_protocol_descriptor = 2,
+			parse_payload_failed = 3,
+		};
+
+		inline boost::system::error_code make_error_code(errc_t e)
+		{
+			return boost::system::error_code(static_cast<int>(e), tinyrpc::error_category());
+		}
+	}
+}
+
+namespace boost {
+	namespace system {
+		template <>
+		struct is_error_code_enum<tinyrpc::errc::errc_t>
+		{
+			static const bool value = true;
+		};
+
+	} // namespace system
+} // namespace boost
+
+namespace tinyrpc {
+	namespace detail {
+
+		class error_category_impl
+			: public boost::system::error_category
+		{
+			virtual const char* name() const noexcept
+			{
+				return "TinyRPC";
+			}
+
+			virtual std::string message(int e) const
+			{
+				switch (e)
+				{
+				case errc::parse_rpc_service_ptl_failed:
+					return "Parse protobuf rpc_service_ptl failed";
+				case errc::unknow_protocol_descriptor:
+					return "unknow protocol descriptor";
+				case errc::parse_payload_failed:
+					return "Parse protobuf payload failed";
+				default:
+					return "Unknown TinyRPC error";
+				}
+			}
+		};
+	}
+}
--- include/tinyrpc/rpc_websocket_service.hpp
+++ include/tinyrpc/rpc_websocket_service.hpp
@@ -16,88 +16,12 @@
 #include <shared_mutex>
 #include <type_traits>
 
-#include "boost/system/system_error.hpp"
-#include "boost/system/error_code.hpp"
-
 #include "boost/beast/core.hpp"
 #include "boost/beast/websocket.hpp"
 
 #include "rpc_service_ptl.pb.h"
 
-namespace tinyrpc {
-
-	//////////////////////////////////////////////////////////////////////////
-	namespace detail {
-		class error_category_impl;
-	}
-
-	template<class error_category>
-	const boost::system::error_category& error_category_single()
-	{
-		static error_category error_category_instance;
-		return reinterpret_cast<const boost::system::error_category&>(error_category_instance);
-	}
-
-	inline const boost::system::error_category& error_category()
-	{
-		return error_category_single<detail::error_category_impl>();
-	}
-
-	namespace errc {
-		enum errc_t
-		{
-			parse_rpc_service_ptl_failed = 1,
-			unknow_protocol_descriptor = 2,
-			parse_payload_failed = 3,
-		};
-
-		inline boost::system::error_code make_error_code(errc_t e)
-		{
-			return boost::system::error_code(static_cast<int>(e), tinyrpc::error_category());
-		}
-	}
-}
-
-namespace boost {
-	namespace system {
-		template <>
-		struct is_error_code_enum<tinyrpc::errc::errc_t>
-		{
-			static const bool value = true;
-		};
-
-	} // namespace system
-} // namespace boost
-
-namespace tinyrpc {
-	namespace detail {
-
-		class error_category_impl
-			: public boost::system::error_category
-		{
-			virtual const char* name() const noexcept
-			{
-				return "TinyRPC";
-			}
-
-			virtual std::string message(int e) const
-			{
-				switch (e)
-				{
-				case errc::parse_rpc_service_ptl_failed:
-					return "Parse protobuf rpc_service_ptl failed";
-				case errc::unknow_protocol_descriptor:
-					return "unknow protocol descriptor";
-				case errc::parse_payload_failed:
-					return "Parse protobuf payload failed";
-				default:
-					return "Unknown TinyRPC error";
-				}
-			}
-		};
-	}
-}
-
+#include "rpc_error_code.hpp"
 
 namespace tinyrpc {
 
